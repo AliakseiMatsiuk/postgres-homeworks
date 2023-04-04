d7b7ac609abac793203559c6f6ee3da02f952a1d
Matsiuk Aliaksei <murryaa@gmai.com>
2023-04-04 20:30:32 +0300

    first commit

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,30 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+# connect to db
+with psycopg2.connect(host='localhost',database='north',user='postgres',password='5432') as conn:
+    with conn.cursor() as cur:
+
+        with open("north_data\customers_data.csv") as customers_file, \
+             open("north_data\employees_data.csv") as employees_file, \
+             open("north_data\orders_data.csv") as orders_file:
+
+            customers_data = csv.reader(customers_file)
+            next(customers_data)
+            employees_data = csv.reader(employees_file)
+            next(employees_data)
+            orders_data = csv.reader(orders_file)
+            next(orders_data)
+
+            for customers in customers_data:
+                cur.execute("INSRT INTO customers VALUES (%s, %s, %s)", customers)
+
+            for employee in employees_data:
+                cur.execute("INSRT INTO employees VALUES (%s, %s, %s, %s, %s)", employee)
+
+            for order in orders_data:
+                cur.execute("INSRT INTO orders VALUES (%s, %s, %s, %s, %s)", order)
+
+conn.close()
+
